f1cb7a08583144d44190e0d1ef3ae24579b3d216
MuhRidhaIlahi <60900121056@uin-alauddin.ac.id>
2023-11-23 12:23:09 +0800

    testing

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -1,75 +1,14 @@
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html lang="ID">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>HMJ</title>
     <link rel="stylesheet" href="css\style.css">
     <link rel="stylesheet" href="boostrap\css\bootstrap.css">
   </head>
   <body>
-    <nav class="navbar fixed-top navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/berita">Berita</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/pengurus">Pengurus</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown link
-                </a>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Features</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Pricing</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown link
-                </a>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+    @include('partials.navbar')
       <div class="container mt-5">
           @yield('container')      
     </div>
--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,29 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('user/beranda',);
+    return view('user/beranda',[
+        "title" => "beranda"
+    ]);
 });
 
 Route::get('/berita', function () {
-    return view('user/berita',);
+    return view('user/berita',[
+        "title" => "berita"
+    ]);
 });
 
 Route::get('/pengurus', function () {
-    return view('user/pengurus',);
+    return view('user/pengurus',[
+        "title" => "pengurus"
+    ]);
 });
+
+Route::get('/tentang', function () {
+    return view('user/tentang',[
+        "title" => "tentang"
+    ]);
+});
+
+
 
 
